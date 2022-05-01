5e1a2c34f626c14da401dfb617061ee1779c36d6
Samkiroko <kirokodev@gmail.com>
2022-05-01 12:49:08 +0300

    Refactor to addEventListener

--- .history/main_20220501124151.js
+++ .history/main_20220501124151.js
@@ -1,10 +1,6 @@
 // Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
 buttonEl = document.getElementById('input-btn')
 
-// function saveInput() {
-//   console.log('button clicked')
-// }
-
-buttonEl.addEventListener('click', () => {
+function saveInput() {
   console.log('button clicked')
-})
+}
